75fcccc31d94210e65fca92fb2c5d9fa1f84dbcf
elthell <eleanor.thell@gmail.com>
2023-07-25 19:59:48 -0500

    add forecast display + remove wireframe

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ searchBtn.addEventListener("click", function (event) {
   // variable to capture entered city
   let cityInput = document.getElementById("city-input").value;
   localStorage.setItem("input", cityInput);
-  console.log(localStorage.getItem("input"))
   // trigger function for city search if a city was entered
   if (cityInput) {
     getCityCoords(cityInput);
@@ -29,25 +28,24 @@ searchBtn.addEventListener("click", function (event) {
 
 // function to save city searches to field
 let savedSearches = function () {
+  // create button for each saved city search
+  let prevCity = document.createElement("button");
+  prevCity.classList = "btn btn-outline-primary";
+  prevCity.textContent = localStorage.getItem("input");
+  searchResults.appendChild(prevCity);
 
-    // create button for each saved city search
-    let prevCity = document.createElement("button");
-    prevCity.classList = "btn btn-outline-primary"
-    prevCity.textContent = localStorage.getItem("input")
-    searchResults.appendChild(prevCity);
+  //event listener to retrigger that cities search
+  prevCity.addEventListener("click", function (event) {
+    event.preventDefault();
 
-    //event listener to retrigger that cities search
-    prevCity.addEventListener("click", function(event){
-        event.preventDefault();
+    let cityInput = prevCity.textContent;
+    getCityCoords(cityInput);
 
-        let cityInput = prevCity.textContent;
-        getCityCoords(cityInput);
-
-        document.getElementById("city-input").value = "";
-        cityForecast.textContent = "";
-        cityWeather.textContent = "";
-    })
-}
+    document.getElementById("city-input").value = "";
+    cityForecast.textContent = "";
+    cityWeather.textContent = "";
+  });
+};
 
 // function to get city coords
 let getCityCoords = function (input) {
@@ -85,11 +83,17 @@ let getCityWeather = function (lat, lon) {
     .then(function (data) {
       // create elements to display city weather
       let cityName = document.createElement("h3");
+
+      // get time and convert from unix
       let time = new Date(data.dt * 1000);
       cityName.textContent = data.name + " (" + time.toDateString() + ")";
+
+      // get img
       let cityImg = document.createElement("img");
       cityImg.src =
         "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+
+      // other values
       let cityTemp = document.createElement("p");
       cityTemp.textContent = "Temp: " + data.main.temp + "°F";
       let cityHumid = document.createElement("p");
@@ -98,7 +102,7 @@ let getCityWeather = function (lat, lon) {
       cityWind.textContent = "Wind: " + data.wind.speed + " mph";
 
       // give container a darker bg to display weather images
-      cityWeather.classList.add("bg-secondary", "bg-opacity-25", "rounded")
+      cityWeather.classList.add("bg-secondary-subtle", "bg-gradient", "rounded");
 
       // append to weather container
       cityName.appendChild(cityImg);
@@ -112,7 +116,7 @@ let getCityWeather = function (lat, lon) {
 // get city weather forecast
 let getCityForecast = function (lat, lon) {
   let apiUrl =
-    "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
     lat +
     "&lon=" +
     lon +
@@ -123,9 +127,56 @@ let getCityForecast = function (lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
 
-      // list of days we want forecasted
-      forecastDays = [];
+      // select middle of each date for best average values
+      let forecastDays = [
+        data.list[4],
+        data.list[12],
+        data.list[20],
+        data.list[28],
+        data.list[36],
+      ];
+      for (var i = 0; i < forecastDays.length; i++) {
+        // make card for each day
+        let forecastCard = document.createElement("div");
+        forecastCard.classList.add(
+          "col",
+          "bg-primary-subtle",
+          "bg-gradient",
+          "rounded",
+          "text-center",
+          "p-2",
+          "m-3"
+        );
+        cityForecast.appendChild(forecastCard);
+
+        // get value for time, stringify, set
+        let time = new Date(forecastDays[i].dt_txt);
+        let forecastDate = document.createElement("h4");
+        forecastDate.classList.add("card-title")
+        forecastDate.textContent = time.toDateString();
+        forecastCard.appendChild(forecastDate);
+
+        // get img
+        let forecastImg = document.createElement("img");
+        forecastImg.src =
+          "https://openweathermap.org/img/wn/" +
+          forecastDays[i].weather[0].icon +
+          ".png";
+          forecastCard.appendChild(forecastImg)
+
+        // other values from array
+        let forecastTemp = document.createElement("p");
+        forecastTemp.textContent = "Temp: " + forecastDays[i].main.temp + "°F";
+        forecastCard.appendChild(forecastTemp);
+
+        let forecastHumid = document.createElement("p");
+        forecastHumid.textContent = "Humidity: " + forecastDays[i].main.humidity + "%";
+        forecastCard.appendChild(forecastHumid);
+
+        let forecastWind = document.createElement("p");
+        forecastWind.textContent = "Wind: " + forecastDays[i].wind.speed + "mph";
+        forecastCard.appendChild(forecastWind);
+      }
     });
 };
